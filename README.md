a56fbba51eeff9d3bd4e8cf58e4f7e3b10ed45a9
teojiesern <teojiesern42@gmail.com>
2024-02-08 12:28:26 +0800

    ignore this, prettify things

--- README.md
+++ README.md
@@ -1,29 +1,30 @@
 # Template for WIA2007 Finals
+
 Either use the created template while creating the app, or perform the following GitHub commands to get to know better on what changed to allow for the creation of certain template
 
 First perform:
+
 ```
 git log
 ```
+
 This should let you see all the commits that contribute to a certain template being created, pick the template that you are interested in, copy the commit hash, and perform the following:
+
 ```
 git checkout <CommitHash>
 ```
+
 Replace the `<CommitHash>` with your commit of choice followed by:
+
 ```
 git reset --soft HEAD~
 ```
+
 open in VSCode and see the files changed. After inspection is done, simply do the following to reset to the latest version and perform the previous steps again when necessary. Run the command to make the repo up to date
+
 ```
 git reset
 git checkout .
 git clean -df
 git checkout master
 ```
-
-## Sequence of creation and changes to take note
-
--   Activity
-    -   ActivityTemplate.java
-    -   AndroidManifest.xml
-    -   MainActivity.java
--- app/src/main/java/com/example/practical_3/SharedPreferenceTemplate.java
+++ app/src/main/java/com/example/practical_3/SharedPreferenceTemplate.java
@@ -4,14 +4,18 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SharedPreferenceTemplate {
-//     this is the read for shared preference
-//    SharedPreferences preferences = getActivity().getSharedPreferences(preference_constant.pUserInfo, Context.MODE_PRIVATE);
-//    String userId = preferences.getString(preference_constant.pUserId, "");
+    // this is the read for shared preference
+    // SharedPreferences preferences =
+    // getActivity().getSharedPreferences(preference_constant.pUserInfo,
+    // Context.MODE_PRIVATE);
+    // String userId = preferences.getString(preference_constant.pUserId, "");
 
-//     this is the write
-//    SharedPreferences preferences = getActivity().getSharedPreferences(preference_constant.pUserInfo, Context.MODE_PRIVATE);
-//    SharedPreferences.Editor spEditor = preferences.edit();
-//    spEditor.putBoolean(preference_constant.pIsLogin,true);
-//    spEditor.putString(preference_constant.pUserId,userId);
-//    spEditor.commit();
+    // this is the write
+    // SharedPreferences preferences =
+    // getActivity().getSharedPreferences(preference_constant.pUserInfo,
+    // Context.MODE_PRIVATE);
+    // SharedPreferences.Editor spEditor = preferences.edit();
+    // spEditor.putBoolean(preference_constant.pIsLogin,true);
+    // spEditor.putString(preference_constant.pUserId,userId);
+    // spEditor.commit();
 }
